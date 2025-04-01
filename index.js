9e085e4babf8fb0185f7a64215270c39c871f09f
Akash4056 <asaha8313@gmail.com>
2025-04-01 16:41:56 +0530

    initialize server with Express, add .gitignore, and set up package.json

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+const app = express();
+dotenv.config();
+const PORT = process.env.PORT || 5000;
+const MONGO_URI = process.env.MONGO_URI
+
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
