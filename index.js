82c823e450e572840a456e5b3b72ce232c36c420
Akash4056 <asaha8313@gmail.com>
2025-04-01 16:43:02 +0530

    add a simple Hello World route to the server

--- index.js
+++ index.js
@@ -13,6 +13,11 @@ const MONGO_URI = process.env.MONGO_URI
 app.use(cors());
 app.use(express.json());
 
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+}
+);
+
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
