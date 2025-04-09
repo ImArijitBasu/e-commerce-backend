64f92889aab49329470b060ee7b25ea22872d8db
Akash4056 <asaha8313@gmail.com>
2025-04-09 23:48:57 +0530

    feat: implement product management routes and controller functions

--- models/Product.js
+++ models/Product.js
@@ -1,19 +1,17 @@
-import { Schema, model, models } from 'mongoose';
+import mongoose from 'mongoose';
 
-const userSchema = new Schema({
+const { Schema, model, models } = mongoose;
+
+const productSchema = new Schema({
+  seller: { type: Schema.Types.ObjectId, ref: 'User', required: true },
   name: { type: String, required: true },
-  email: { type: String, unique: true, required: true },
-  password: { type: String },
-  role: { type: String, enum: ['user', 'seller', 'admin'], default: 'user' },
-  image: { type: String },
-  phone: String,
-  address: String,
+  description: String,
+  price: { type: Number, required: true },
+  stock: { type: Number, default: 0 },
+  category: { type: Schema.Types.ObjectId, ref: 'Category' },
+  imageUrl: [String],
+  rating: { type: Number, default: 0 },
   createdAt: { type: Date, default: Date.now },
-
-  // Relationships
-  orders: [{ type: Schema.Types.ObjectId, ref: 'Order' }], // User's orders
-  reviews: [{ type: Schema.Types.ObjectId, ref: 'Review' }], // User's reviews
-  recommendations: { type: Schema.Types.ObjectId, ref: 'Recommendation' }, // User's recommendations
 });
 
-export const User = models.User || model('User', userSchema);
+export const Product = models.Product || model('Product', productSchema);
